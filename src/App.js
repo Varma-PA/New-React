c80f3ad2db005d079d836626f9e019f987902ea0
Achyuth Varma <sree-achyutha.penmetcha@capgemini.com>
2020-09-16 17:07:49 +0530

    Added Dockerfile

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with AWS Amplify
+          Deploying App on AWS EC2
         </a>
       </header>
     </div>
